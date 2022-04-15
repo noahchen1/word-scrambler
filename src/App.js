4840024f821141dfc98554b7a81732b39faa281a
swaglord2021 <nuoya1996@gmail.com>
2022-04-15 15:58:32 -0400

    Update with all functionalities complete

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import InputBox from './InputBox';
 
 export default function App() {
 
+  const [apiCounter, setAPICounter] = useState(1)
   const [sentence, setSentence] = useState(' ')
   const [sentenceArr, setSentenceArr] = useState([])
   const [scrambledSentence, setScrambledSentence] = useState('')
@@ -16,19 +17,33 @@ export default function App() {
 
   const [count, setCount] = useState(0)
   const [inputArr, setInputArr] = useState([])
+  const [clearInputs, setClearInputs] = useState(false)
 
   const [nextBottom, setNextBottom] = useState('none')
-  
+
+  const [mainUi, setMainUi] = useState('flex')
+  const [youWin, setYouWin] = useState('none')
+
+
   // Making a request and store it in 'sentence' state,
   // Setting trigger to true to signify the compeltion of GET request
 
   useEffect(() => {
-    axios.get('https://api.hatchways.io/assessment/sentences/1')
+    axios.get(`https://api.hatchways.io/assessment/sentences/${apiCounter}`)
       .then(res => {
         setSentence(res.data.data.sentence)
       })
       setTrigger(true)
-  }, [])
+
+      if (apiCounter === 11) {
+        setMainUi('none')
+        setYouWin('block')
+      }
+
+  }, [apiCounter])
+
+
+
 
   useEffect(() => {
     // stop if request is not finished
@@ -59,6 +74,7 @@ export default function App() {
 
   }, [sentence])
 
+
   useEffect(() => {
     const inputSentence = inputArr.join('').toLowerCase()
     if (inputSentence == sentence.toLowerCase()) {
@@ -67,113 +83,145 @@ export default function App() {
 
   }, [inputArr])
 
+
   let maxLength = sentence.split('').length - 1
 
 
   return (
     <div className='scrambled-word' style={{backgroundColor: '#e1e1e1', height: '100vh', width: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-    <div style={{display: 'flex', alignItems: 'center', flexDirection:'column', backgroundColor: 'white', height: '90%', width: '80%', maxWidth: '1000px', minWidth: '390px', borderRadius: '15px'}}>
 
-      <div style={{display:'flex', flexDirection: 'column', justifyContent:'space-around', width: '60%', minWidth: '300px', height: '40%'}}>
-        <div style={{color: 'rgb(57, 135, 201)', fontWeight: '800', fontSize: '2.2rem', textAlign: 'center'}}>
-          {scrambledSentence}
+      <div style={{display: `${youWin}`, position: 'absolute', height: '50%', width: '40%', maxWidth: '1000px', minWidth: '390px'}}>
+        <div style={{display:'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'white', borderRadius: '15px', height: '50px'}}>
+          You Win!
         </div>
-        <div style={{textAlign: 'center', fontWeight: '800', fontSize:'1.2rem', color: 'rgba(0, 0, 0, 0.7)'}}>Guess the sentence! Starting typing</div>
-        <div style={{textAlign: 'center', fontWeight: '800', fontSize:'1.2rem', color: 'rgba(0, 0, 0, 0.7)'}}>The yellow blocks are meant for spaces</div>
-        <div style={{textAlign: 'center', fontWeight: '600', fontSize: '1.8rem'}}>Score: 0</div>
       </div>
 
-      <div style={{width: '80%', height: '50%'}}>
-        {sentenceArr.map((word, index) => {
-          let lettersArr = word.split('') 
-          let wordIndex = index
-          if (index < sentenceArr.length - 1) {
-            lettersArr.push(' ')
-            var inputWidth = parseInt(1 / lettersArr.length * 100) - 2
 
-            return(
-              <div style={{margin: '5px 0', display: 'flex', justifyContent: 'space-between'}}>
-                {lettersArr.map((letter, index) => {
-                  return(
-                    index < lettersArr.length - 1 ?
-                    <InputBox 
-                      letter={letter}
-                      inputWidth={inputWidth}
-                      wordIndex={wordIndex}
-                      sentenceArr={sentenceArr}
-                      count={count}
-                      setCount={setCount}
-                      index={index}
-                      maxLength={maxLength}
-                      inputArr={inputArr}
-                      setInputArr={setInputArr}
-                      backgroundColor='#e1e1e1'
-                    />
-                    :
-                    <InputBox 
-                      letter={letter}
-                      inputWidth={inputWidth}
-                      wordIndex={wordIndex}
-                      sentenceArr={sentenceArr}
-                      count={count}
-                      setCount={setCount}
-                      index={index}
-                      maxLength={maxLength}
-                      inputArr={inputArr}
-                      setInputArr={setInputArr}
-                      backgroundColor='#ffb74d'
-                    />
-                  )
-                })}
+      <div style={{display: `${mainUi}`, alignItems: 'center', flexDirection:'column', backgroundColor: 'white', height: 'auto', width: '80%', maxWidth: '1000px', minWidth: '390px', borderRadius: '15px'}}>
+
+        <div style={{display:'flex', flexDirection: 'column', justifyContent:'space-around', width: '60%', minWidth: '300px', height: '40%', margin: '5% 0'}}>
+          <div style={{color: 'rgb(57, 135, 201)', fontWeight: '800', fontSize: '2.2rem', textAlign: 'center', margin: '3% 0'}}>
+            {scrambledSentence}
+          </div>
+          <div style={{textAlign: 'center', fontWeight: '800', fontSize:'1.2rem', color: 'rgba(0, 0, 0, 0.7)', margin: '3% 0'}}>Guess the sentence! Starting typing</div>
+          <div style={{textAlign: 'center', fontWeight: '800', fontSize:'1.2rem', color: 'rgba(0, 0, 0, 0.7)', margin: '3% 0'}}>The yellow blocks are meant for spaces</div>
+          <div style={{textAlign: 'center', fontWeight: '600', fontSize: '1.8rem', margin: '3% 0'}}>{`Score: ${apiCounter - 1}`}</div>
+        </div>
+
+        <div style={{width: '80%', height: '40vh'}}>
+          {sentenceArr.map((word, index) => {
+            let lettersArr = word.split('') 
+            let wordIndex = index
+            if (index < sentenceArr.length - 1) {
+              lettersArr.push(' ')
+
+              var inputWidth = parseInt(1 / lettersArr.length * 100) - 2
+
+              return(
+                <div style={{margin: '10px 0', display: 'flex', justifyContent: 'space-between', height: '20%'}}>
+                  {lettersArr.map((letter, index) => {
+                    return(
+                      index < lettersArr.length - 1 ?
+                      <InputBox 
+                        letter={letter}
+                        inputWidth={inputWidth}
+                        wordIndex={wordIndex}
+                        sentenceArr={sentenceArr}
+                        count={count}
+                        setCount={setCount}
+                        index={index}
+                        maxLength={maxLength}
+                        inputArr={inputArr}
+                        setInputArr={setInputArr}
+                        clearInputs={clearInputs}
+                        setClearInputs={setClearInputs}
+                        nextButton={nextBottom}
+                        backgroundColor='#e1e1e1'
+                      />
+                      :
+                      <InputBox 
+                        letter={letter}
+                        inputWidth={inputWidth}
+                        wordIndex={wordIndex}
+                        sentenceArr={sentenceArr}
+                        count={count}
+                        setCount={setCount}
+                        index={index}
+                        maxLength={maxLength}
+                        inputArr={inputArr}
+                        setInputArr={setInputArr}
+                        clearInputs={clearInputs}
+                        setClearInputs={setClearInputs}
+                        nextButton={nextBottom}
+                        backgroundColor='#ffb74d'
+                      />
+                    )
+                  })}
+                </div>
+              )
+            } else {
+
+              var inputWidth = parseInt(1 / lettersArr.length * 100) - 2
+              return (
+                <div style={{margin: '5px 0', display: 'flex', justifyContent: 'space-between', height: '20%', textAlign: 'center'}}>
+                  {lettersArr.map((letter, index) => {
+                    return (
+                      index < lettersArr.length - 1 ?
+                      <InputBox 
+                        letter={letter}
+                        inputWidth={inputWidth}
+                        wordIndex={wordIndex}
+                        sentenceArr={sentenceArr}
+                        count={count}
+                        setCount={setCount}
+                        index={index}
+                        maxLength={maxLength}
+                        inputArr={inputArr}
+                        setInputArr={setInputArr}
+                        clearInputs={clearInputs}
+                        setClearInputs={setClearInputs}
+                        nextButton={nextBottom}
+                        backgroundColor='#e1e1e1'
+                      />
+                      :
+                      <InputBox 
+                        letter={letter}
+                        inputWidth={inputWidth}
+                        wordIndex={wordIndex}
+                        sentenceArr={sentenceArr}
+                        count={count}
+                        setCount={setCount}
+                        index={index}
+                        maxLength={maxLength}
+                        inputArr={inputArr}
+                        setInputArr={setInputArr}
+                        clearInputs={clearInputs}
+                        setClearInputs={setClearInputs}
+                        nextButton={nextBottom}
+                        backgroundColor='#e1e1e1'
+                      />
+                    )
+                  })}
               </div>
-            )
-          } else {
-            return (
-              <div style={{margin: '5px 0', display: 'flex', justifyContent: 'space-between'}}>
-                {lettersArr.map((letter, index) => {
-                  return (
-                    index < lettersArr.length - 1 ?
-                    <InputBox 
-                      letter={letter}
-                      inputWidth={inputWidth}
-                      wordIndex={wordIndex}
-                      sentenceArr={sentenceArr}
-                      count={count}
-                      setCount={setCount}
-                      index={index}
-                      maxLength={maxLength}
-                      inputArr={inputArr}
-                      setInputArr={setInputArr}
-                      backgroundColor='#e1e1e1'
-                    />
-                    :
-                    <InputBox 
-                      letter={letter}
-                      inputWidth={inputWidth}
-                      wordIndex={wordIndex}
-                      sentenceArr={sentenceArr}
-                      count={count}
-                      setCount={setCount}
-                      index={index}
-                      maxLength={maxLength}
-                      inputArr={inputArr}
-                      setInputArr={setInputArr}
-                      backgroundColor='#e1e1e1'
-                    />
-                  )
-                })}
-            </div>
-            )}})}
-      </div>
+              )}})}
+        </div>
 
-      <div style={{display: `${nextBottom}`}}>
-        <button
-          onClick={e =>{setNextBottom('none')}}
-        >
-          Next
-        </button>
-      </div>
- 
+        <div style={{display: `${nextBottom}`, marginBottom: '20px'}}>
+          <button
+            id='next-button'
+            onClick={e =>{
+              setNextBottom('none')
+              if (apiCounter <= 10) {
+                setAPICounter(apiCounter + 1)
+              }
+              setClearInputs(true)
+            }}
+            style={{padding: '10px 25px', backgroundColor: '#4caf50', border: 'none', color: 'white', fontSize: '1.5rem'}}
+          >
+            Next
+          </button>
+        </div>
+  
     </div>
   </div>
   )
--- src/InputBox.js
+++ src/InputBox.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 
-export default function InputBox({backgroundColor, letter, inputWidth, wordIndex, sentenceArr, index, count, setCount, maxLength, inputArr, setInputArr}) {
+export default function InputBox({backgroundColor, letter, inputWidth, wordIndex, sentenceArr, index, count, setCount, maxLength, setInputArr, clearInputs, setClearInputs, nextButton}) {
 
     const [currentInput, setCurrentInput] = useState('')
 
@@ -17,43 +17,61 @@ export default function InputBox({backgroundColor, letter, inputWidth, wordIndex
 
 
     const handleKeyPress = e => {
+
         setInputArr(prevState => [...prevState, currentInput])
+
+
         if(count < maxLength) {
             setCount(currentIndex + 1)
         }
-        if (e.key === 'Backspace') {
+
+        if (e.key === 'Backspace' && count < maxLength) {
             setInputArr(prevState => prevState.slice(0, -2))
             setCount(currentIndex - 1)
             setCurrentInput('')
         }
+
+        if (e.key === 'Backspace' && count === maxLength) {
+            setInputArr(prevState => prevState.slice(0, -3))
+            setCount(currentIndex - 1)
+            setCurrentInput('')
+        }
+
+        if (e.key === 'Enter' && nextButton === 'block') {
+            const btn = document.getElementById('next-button')
+            btn.click()
+        }
     }
 
 
-    const nextInput = document.querySelector(`input[name=input-${count}]`)
+
 
     useEffect(()=> {
+        const nextInput = document.querySelector(`input[name=input-${count}]`)
         if (nextInput !== null) {
             nextInput.focus()
             nextInput.value = ''
         }
 
-        
     }, [count])
 
-
-
+    useEffect(() => {
+        setCurrentInput('')
+        setClearInputs(false)
+        setCount(0)
+        setInputArr([])
+    }, [clearInputs])
 
     return (
         <span style={{width: `${inputWidth}%`}}>
-            <input  style={{width: '100%', backgroundColor: `${letter.toUpperCase() === currentInput.toUpperCase() ? "#4caf50" : backgroundColor }`}} 
+            <input  style={{width: '100%', height: '90%', textAlign: 'center', fontSize: '1.8rem', color: 'white', caretColor: 'black', border: 'none', outline: 'none', backgroundColor: `${letter.toUpperCase() === currentInput.toUpperCase() ? "#4caf50" : backgroundColor }`}} 
                     type='text' 
                     value={currentInput} 
                     onChange={onChangeCurrentInput} 
-                    onKeyUpCapture={handleKeyPress}
+                    onKeyUp={handleKeyPress}
                     maxLength={1}
                     disabled={currentIndex !== count}
                     name={`input-${currentIndex}`}
-                    autoFocus={true}
             >      
             </input>
         </span>
