1086edb1ff9ebc68933749e9aed33a1d438882f1
swaglord2021 <nuoya1996@gmail.com>
2022-04-15 22:29:31 -0400

    Added comments

--- src/App.js
+++ src/App.js
@@ -5,24 +5,43 @@ import InputBox from './InputBox';
 
 
 
-
-
 export default function App() {
 
+  // state to store the last route of the API address, which gets upticked when the next button is clicked. 
   const [apiCounter, setAPICounter] = useState(1)
+
+  // state to store the original sentence from the current API call
   const [sentence, setSentence] = useState(' ')
+
+  // state to store the original sentence in an array, separated by each word in the sentence
+  // this is here to facilate getting the index of the current input 
   const [sentenceArr, setSentenceArr] = useState([])
+
+  // state to store the scrambled sentence 
   const [scrambledSentence, setScrambledSentence] = useState('')
+
+  // trigger to make sure code dependent on the API gets executed only after the get request is completed
   const [trigger, setTrigger] = useState(false)
 
+  // state to store the count number, which is upticked when an input is entered, downticked vice versa. 
+  // this is here as a referrence to the index of the current input so all inputs are disabled except for the current input. 
   const [count, setCount] = useState(0)
+
+  // state to store the inputs. This is here to determine if all inputs entered match with the original sentence
   const [inputArr, setInputArr] = useState([])
+
+  // state used to clear all inputs when the next button is clicked.
   const [clearInputs, setClearInputs] = useState(false)
 
+  // state to control the visbility of the next button
   const [nextBottom, setNextBottom] = useState('none')
 
+  // state to control the visibility of the UI
   const [mainUi, setMainUi] = useState('flex')
+
+  // state to control the visibility of the "you win" window
   const [youWin, setYouWin] = useState('none')
+
 
 
   // Making a request and store it in 'sentence' state,
@@ -35,6 +54,7 @@ export default function App() {
       })
       setTrigger(true)
 
+      // condition to check if we have reached the last sentence from the API
       if (apiCounter === 11) {
         setMainUi('none')
         setYouWin('block')
@@ -69,13 +89,16 @@ export default function App() {
     // store scrambled sentence in 'scrambledSentence' state for east of access
     setScrambledSentence(scrambledSentece.join(' '))
     
-    //
+
     setSentenceArr(sentence.split(' '))
 
   }, [sentence])
 
 
+
   useEffect(() => {
+    // join inputs entered to a string and compare it against the orignal sentence. 
+    // next button is set to visible only when the inputs are idential to the orignal sentence
     const inputSentence = inputArr.join('').toLowerCase()
     if (inputSentence == sentence.toLowerCase()) {
       setNextBottom('block')
@@ -84,6 +107,8 @@ export default function App() {
   }, [inputArr])
 
 
+  // initiating a variable to store the length of the original sentence in letters array. 
+  // this is used as a constraint for the input component
   let maxLength = sentence.split('').length - 1
 
 
@@ -108,19 +133,24 @@ export default function App() {
           <div style={{textAlign: 'center', fontWeight: '600', fontSize: '1.8rem', margin: '3% 0'}}>{`Score: ${apiCounter - 1}`}</div>
         </div>
 
+
         <div style={{width: '80%', height: '40vh'}}>
           {sentenceArr.map((word, index) => {
             let lettersArr = word.split('') 
             let wordIndex = index
+            
+            // adding an extra space to each word except for the last word as required by the instruction
             if (index < sentenceArr.length - 1) {
               lettersArr.push(' ')
 
+              // calculating the length of each input based on the length of each word it is in with the extra space at the end
               var inputWidth = parseInt(1 / lettersArr.length * 100) - 2
 
               return(
                 <div style={{margin: '10px 0', display: 'flex', justifyContent: 'space-between', height: '20%'}}>
                   {lettersArr.map((letter, index) => {
                     return(
+                      // checking if the input is the extra space at the end, which needs to be set to have a different background color
                       index < lettersArr.length - 1 ?
                       <InputBox 
                         letter={letter}
@@ -160,13 +190,12 @@ export default function App() {
                 </div>
               )
             } else {
-
+              // calculating the length of each input based on the length of the last word of the sentence
               var inputWidth = parseInt(1 / lettersArr.length * 100) - 2
               return (
                 <div style={{margin: '5px 0', display: 'flex', justifyContent: 'space-between', height: '20%', textAlign: 'center'}}>
                   {lettersArr.map((letter, index) => {
                     return (
-                      index < lettersArr.length - 1 ?
                       <InputBox 
                         letter={letter}
                         inputWidth={inputWidth}
@@ -183,37 +212,24 @@ export default function App() {
                         nextButton={nextBottom}
                         backgroundColor='#e1e1e1'
                       />
-                      :
-                      <InputBox 
-                        letter={letter}
-                        inputWidth={inputWidth}
-                        wordIndex={wordIndex}
-                        sentenceArr={sentenceArr}
-                        count={count}
-                        setCount={setCount}
-                        index={index}
-                        maxLength={maxLength}
-                        inputArr={inputArr}
-                        setInputArr={setInputArr}
-                        clearInputs={clearInputs}
-                        setClearInputs={setClearInputs}
-                        nextButton={nextBottom}
-                        backgroundColor='#e1e1e1'
-                      />
-                    )
+                      )
                   })}
               </div>
               )}})}
         </div>
 
+        {/*next button*/}
         <div style={{display: `${nextBottom}`, marginBottom: '20px'}}>
           <button
             id='next-button'
             onClick={e =>{
+              // Setting button visibility off after a click event
               setNextBottom('none')
+              // Fetching for the next setence 
               if (apiCounter <= 10) {
                 setAPICounter(apiCounter + 1)
               }
+              // clearing all inputs
               setClearInputs(true)
             }}
             style={{padding: '10px 25px', backgroundColor: '#4caf50', border: 'none', color: 'white', fontSize: '1.5rem'}}
