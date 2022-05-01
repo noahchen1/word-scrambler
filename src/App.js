8c206f78c2acd348c3b94417f1c7d095fff9bbae
swaglord2021 <nuoya1996@gmail.com>
2022-05-01 17:33:37 -0400

    Daily check-in

--- src/App.js
+++ src/App.js
@@ -272,6 +272,7 @@ export default function App() {
                   })}
                 </div>
               );
+              
             } else {
               // calculating the length of each input based on the length of the last word of the sentence
               var inputWidth = parseInt((1 / lettersArr.length) * 100) - 2;
